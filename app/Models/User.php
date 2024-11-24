4f7f846290efff9ecdb5b3c61d77cc10a1307d50
Andrys Salazar <69359960+sirzan@users.noreply.github.com>
2024-11-23 19:07:58 -0500

    Add initial models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,21 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class, 'project_user');
+    }
+
 }
